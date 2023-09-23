330d698e1243aadc38ae5ee503302539a0092b51
RoblesYesica <karen.yesica.robles@gmail.com>
2023-09-23 12:21:56 -0300

    first carousel version

---
+++ project1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Slider from './slider';
+import Nvar from './nvar';
+
+function App() {
+  return (
+    <div className="App">  
+       <Nvar/>
+       <Slider/>
+        
+       
+    </div>
+  );
+}
+
+export default App;
